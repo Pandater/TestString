7e31986e3a72abe91bc23a18fe7bac8c795c13b4
Pandater <Hubert96@onet.pl>
2023-11-15 09:36:01 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        // TODO) 1 string wpisany, ostatnie 2 na przód (min 3 znaki). jeśli nie zostawiasz
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Wprowadź dowolny Łańcuch: ");
+        String lancuch = scan1.nextLine();
+        if(lancuch.length() > 2) {
+            String lancuch1 = lancuch.substring(0, lancuch.length()-2);
+            String lancuch2 = lancuch.substring(lancuch.length()-2);
+            System.out.println(lancuch2.concat(lancuch1));
+        }
+        else{
+            System.out.println(lancuch);
         }
     }
+}
